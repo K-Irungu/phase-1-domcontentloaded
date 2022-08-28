7c650498310b7b315eaa31742d2991df48c6725d
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-28 22:33:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    replaceParagraph("This is really cool!");
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+
+  function replaceParagraph(newText) {
+    let paragraph = document.querySelector('#text');
+    paragraph.textContent = newText;
+  }
